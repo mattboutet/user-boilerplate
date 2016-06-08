fdb32618a888490adba9cac0c87412f4daef4d64
Matt Boutet <matt@bigroomstudios.com>
2016-06-08 10:44:27 -0400

    Add sample creds file

---
+++ server/credentials-sample.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    mysqlCreds: {
+        user      : 'mysqlUser',
+        password  : 'mysqlPassword'
+    },
+    secrets: {
+        jwtSecret: 'SomethingVerySecret'
+    }
+};
